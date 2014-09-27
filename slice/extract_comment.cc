40b1840b95da20971652a565cd8edad01c4cf92b
liuluheng <872287087@qq.com>
2014-09-27 18:08:47 +0800

    correct lis and extract_comment

--- slice/extract_comment.cc
+++ slice/extract_comment.cc
@@ -3,24 +3,25 @@
 #include <string>
 using namespace std;
 
+//only consider the /* ... */ comment
 vector<string> extract(string pro)
 {
   vector<string> result;
   while (1) {
     size_t splash = pro.find('/');
-    if (splash == std::string::npos)
-      break;
-    if (pro[splash + 1] == '*') {
-      string comment = pro.substr(splash + 2);
-      size_t star = comment.find('*');
-      if (comment[star + 1] == '/') {
-        result.push_back(string(comment, 0,  star--));
-        pro = comment.substr(star + 2);
-        break;
-      } else {
-      }
-    } else {
+    while (pro[splash + 1] != '*'
+        && splash != std::string::npos) {
+      splash = pro.find('/', splash + 1);
     }
+    if (splash == string::npos)
+      break;
+    string comment = pro.substr(splash + 2);
+    size_t star = comment.find('*');
+    while (comment[star + 1] != '/') {
+      star = comment.find('*', star + 1);
+    }
+    result.push_back(string(comment, 0,  star - 1));
+    pro = comment.substr(star + 2);
   }
 
   return result;
@@ -29,7 +30,7 @@ vector<string> extract(string pro)
 
 int main()
 {
-  string str(" /* fuck shit world... */ ");
+  string str("/////* /* fuck shit * * * world... */ heie /* helell wor*/");
 
   auto vec = extract(str);
 
--- slice/lis.cc
+++ slice/lis.cc
@@ -10,6 +10,7 @@ int lis(int A[], int n)
   for (int L = 2; L <= n - 1; L++) {
     for (int i = 1; i <= n - L + 1; i++) {
       int j = i + L -1;
+      //wrong??
       if (A[j] >= A[j -1]) {
         if (L == 2)
           len[j] = 2;
@@ -28,10 +29,29 @@ int lis(int A[], int n)
   return len[n];
 }
 
+int lis2(int A[], int n)
+{
+  int d[n];
+  for (int i = 0; i < n; i++) {
+    d[i] = 1;
+    for (int j = 0; j < i; j++) {
+      if (A[j] <= A[i] && d[j] + 1 > d[i] )
+        d[i] = d[j] + 1;
+    }
+  }
+
+  for (auto i : d)
+    cout << i << " ";
+  cout << endl;
+
+  return d[n - 1];
+}
+
 int main()
 {
-  int A[] = {0, 2, 4, 3, 6, 5, 7, 1};
-  cout << lis(A, 7) << endl;
+  int A[] = {/*-1,*/ 5, 3, 4, 8, 6, 7};
+  cout << lis(A, 6) << endl;
+  cout << lis2(A, 6) << endl;
 
   return 0;
 }
