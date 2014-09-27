fe516ab099a9397780789767aa25c5ae0cbc63e7
liuluheng <872287087@qq.com>
2014-09-27 11:40:31 +0800

    correct searchMatrix

--- leetcode/searchMatrix.cc
+++ leetcode/searchMatrix.cc
@@ -9,16 +9,19 @@ bool searchMatrix(vector<vector<int>> &Matrix, int target)
 
   const size_t rows = Matrix.size();
   const size_t cols = Matrix.front().size();
-
-  int first = 0, last = rows * cols - 1;
+  int first = 0, last = rows * cols;
 
   while (first < last) {
-    int mid = (first + last) / 2;
-    int value = Matrix[mid/rows][mid%cols];
+    int mid = first + (last - first) / 2;
+    int value = Matrix[mid/cols][mid%cols];
+    cout << first << " "
+         << mid << " "
+         << last << " "
+         << value << endl;
     if (value < target)
       first = mid + 1;
     else if (value > target)
-      last = mid; // mid - 1 is wrong... ???
+      last = mid; // mid - 1 is wrong... when search 3
     else
       return true;
   }
@@ -34,7 +37,10 @@ int main()
     { 23, 30, 34, 50}
   };
 
+  cout << searchMatrix(Matrix, 1) << endl;
   cout << searchMatrix(Matrix, 3) << endl;
+  cout << searchMatrix(Matrix, 50) << endl;
+
 
   return 0;
 }
