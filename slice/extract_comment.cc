f73b093f8e2d34312ed4e9cb4690207ee6b7c5c8
liuluheng <872287087@qq.com>
2014-10-15 16:39:04 +0800

    new feature can process //... comment in extract_comment.cc

--- slice/extract_comment.cc
+++ slice/extract_comment.cc
@@ -3,25 +3,35 @@
 #include <string>
 using namespace std;
 
-//only consider the /* ... */ comment
+// consider the /* ... */ and // comments
 vector<string> extract(string pro)
 {
   vector<string> result;
   while (1) {
     size_t splash = pro.find('/');
-    while (pro[splash + 1] != '*'
-        && splash != std::string::npos) {
+    while (splash != std::string::npos
+        && pro[splash + 1] != '*'
+        && pro[splash + 1] != '/') {
       splash = pro.find('/', splash + 1);
     }
     if (splash == string::npos)
       break;
+
     string comment = pro.substr(splash + 2);
-    size_t star = comment.find('*');
-    while (comment[star + 1] != '/') {
-      star = comment.find('*', star + 1);
+    if (pro[splash + 1] == '/') {
+      size_t new_line = comment.find('*');
+      result.push_back(string(comment, 0, new_line - 1));
+      pro = comment.substr(new_line + 2);
     }
-    result.push_back(string(comment, 0,  star - 1));
-    pro = comment.substr(star + 2);
+
+    if (pro[splash + 1] == '*') {
+      size_t star = comment.find('*');
+      while (comment[star + 1] != '/') {
+        star = comment.find('*', star + 1);
+      }
+      result.push_back(string(comment, 0,  star - 1));
+      pro = comment.substr(star + 2);
+    }
   }
 
   return result;
@@ -30,7 +40,8 @@ vector<string> extract(string pro)
 
 int main()
 {
-  string str("/////* /* fuck shit * * * world... */ heie /* helell wor*/");
+  string str("////\n"
+             "/* /* fuck shit * * * world... */ heie /* helell wor*/");
 
   auto vec = extract(str);
 
